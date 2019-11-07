a83a3ac9723a6a4757e165dac1a554c52c4ed67e
fernandoguide <fernando-guide@hotmail.com>
2019-11-07 20:54:09 -0200

    criado o arquivo .jar executavel

--- projeto-datasource/src/main/java/br/com/bandtec/datasource/view/TelaMaqUsuario.java
+++ projeto-datasource/src/main/java/br/com/bandtec/datasource/view/TelaMaqUsuario.java
@@ -389,6 +389,8 @@ public class TelaMaqUsuario extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.dispose();
+        TelaLogin tela = new TelaLogin();
+        tela.setVisible(true);
 
     }//GEN-LAST:event_jButton2ActionPerformed
 
