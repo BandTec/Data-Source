311cd51b2e87984d92d1ac1b1bef0ef72cc9f04e
ThalitaModestogit <thalitamodest14@gmail.com>
2019-11-14 19:44:02 -0300

    Background da tela de login e cadastro

--- SiteEmpresa/data-source/src/Pages/Cadastro/index.js
+++ SiteEmpresa/data-source/src/Pages/Cadastro/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import api from '../../Services/api';
-import { Link } from 'react-router-dom'
+import Particles from 'react-particles-js';
 
 export default function Login({ history }) {
     const [user, setUser] = useState('');
@@ -77,6 +77,118 @@ export default function Login({ history }) {
       </form>
       </div>
       </div>
+      <Particles className="particle"
+        params={{
+          "particles": {
+            "number": {
+              "value": 80,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#0ce3ec"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 4
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 1.542946703372556,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 5,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 9.620472365193136,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 200,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+        }
+        }/>
     </>
   )
 }
--- SiteEmpresa/data-source/src/Pages/Login/index.js
+++ SiteEmpresa/data-source/src/Pages/Login/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import api from '../../Services/api';
 import { Link } from 'react-router-dom'
+import Particles from 'react-particles-js';
+
 
 export default function Login({ history }) {
   const [email, setEmail] = useState('');
@@ -11,7 +13,7 @@ export default function Login({ history }) {
     event.preventDefault();
 
 
-    if (email === '' || senha === '' ) {
+    if (email === '' || senha === '') {
       alert("Preencha todos os campos");
     } else {
       const response = await api.post('/login', { email, senha });
@@ -19,15 +21,15 @@ export default function Login({ history }) {
       if (response.data.data === null) {
         alert("Verifique suas credenciais");
       } else {
-         //const { ID_USUA_CD_USUARIO } = response.data;
+        //const { ID_USUA_CD_USUARIO } = response.data;
         //await localStorage.setItem('_id', _id)
         history.push('/dashboard');
       }
     }
 
 
-    
-   
+
+
     //const { ID_USUA_CD_USUARIO } = response.data;
 
     //localStorage.setItem('user', id);
@@ -36,32 +38,147 @@ export default function Login({ history }) {
   }
 
   return (
-    
+
     <>
-    <div className="container">
+<div className="container">
       <h1 className="h1">DataSource.</h1>
-      <br/>
+      <br />
       <div className="content">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">E-MAIL *</label>
-        <input 
-          id="email" 
-          type="email" 
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-        />
-         <label htmlFor="senha">SENHA *</label>
-        <input 
-          id="senha" 
-          type="password"
-          value={senha}
-          onChange={event => setSenha(event.target.value)}
-        />
-        <>Não possui conta? <Link to="/Cadastro">cadastre-se</Link></>
-        <button className="btn" type="submit">Entrar</button>
-      </form>
-      </div>
-      </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+          <label htmlFor="senha">SENHA *</label>
+          <input
+            id="senha"
+            type="password"
+            value={senha}
+            onChange={event => setSenha(event.target.value)}
+          />
+          <>Não possui conta? <Link to="/Cadastro">cadastre-se</Link></>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+        </div>
+        </div>
+      <Particles className="particle"
+        params={{
+          "particles": {
+            "number": {
+              "value": 75,
+              "density": {
+                "enable": true,
+                "value_area": 800
+              }
+            },
+            "color": {
+              "value": "#0ce3ec"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 4
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 0.5,
+              "random": false,
+              "anim": {
+                "enable": false,
+                "speed": 1.542946703372556,
+                "opacity_min": 0.1,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 5,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": true,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 9.620472365193136,
+              "direction": "none",
+              "random": false,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 200,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+        }
+        }/>
+   
+   
+    
     </>
   )
 }
