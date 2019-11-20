9251bec0ff764c1cb0245fcc0b42044ff3069fa7
fernandoguide <fernnado-guide@hotmail.com>
2019-11-20 08:58:37 -0300

    alteracao no script

---
+++ data-source.bat
@@ -0,0 +1,4 @@
+md C:\DataSource
+copy projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar  C:\DataSource
+dir C:\DataSource
+start projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar
--- script.bat
+++
@@ -1,4 +0,0 @@
-md C:\DataSource
-copy projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar  C:\DataSource
-dir C:\DataSource
-start projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar
