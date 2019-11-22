25e11e62d4691fbefa96656dd440fa081fd28cd5
fernandoguide <fernando-guide@hotmail.com>
2019-11-22 14:02:32 -0300

    atualizando arquivo de instalacao Linux

--- Instalacao-DataSource/arquivos de instalacao/data-source.sh
+++ Instalacao-DataSource/arquivos de instalacao/data-source.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar  /usr/bin 
-cd /usr/bin 
+cp projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar  /usr/lib
+cp data* /usr/lib
+cd /usr/lib
+chmod +x data* 
 java -jar projeto-datasource-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 
